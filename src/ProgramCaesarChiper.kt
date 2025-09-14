0515cf711738c34a35b22b330c4e21bd90585b5a
mzikriamin11 <mzikriamin95@gmail.com>
2025-09-14 10:48:42 +0700

    Commit Pertama

---
+++ src/ProgramCaesarChiper.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+
+
+    //The Caesar cipher
+    // variable initialization
+    val s = "ini pesan original"
+    println("Original message: $s")
+    var message = ""
+    var shift = 1
+
+    // loop iterating over characters
+    for (c in s) {
+        var i = c.toInt()
+        i += shift
+        if (i > 'z'.toInt()) {
+            i -= 26
+        }
+        val char = i.toChar()
+        message += char
+    }
+
+    // printing
+    println("Encrypted message: $message")
+}
